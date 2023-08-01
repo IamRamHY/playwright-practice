623423963e12981cdceb3ceee0449b6b6a6fc8b7
IamRamHY <ramamurthy.h@enlume.com>
2023-08-01 18:16:27 +0530

    Add test case - Verify for page title

--- tests/playwright-website.spec.ts
+++ tests/playwright-website.spec.ts
@@ -1,7 +1,13 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.describe("home page", async () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("https://playwright.dev/");
+  });
+
+  test("Verify for page title", async ({ page }) => {
+    await expect(page).toHaveTitle(
+      "Fast and reliable end-to-end testing for modern web apps | Playwright"
+    );
   });
 });
